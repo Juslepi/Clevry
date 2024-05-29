94354cbd085485c67ec30635ddff23836d819213
Jussi Lepistö <juslepi@gmail.com>
2024-05-29 12:04:04 +0300

    Only run script once

--- paivaraha.py
+++ paivaraha.py
@@ -15,13 +15,12 @@ def copy_to_clipboard(string):
 now = datetime.now()
 date = now.strftime("%d.%m").lstrip("0").replace(".0", ".")
 
-while True:
-    clear_console()
-    kaupunki = input("Kaupunki: ") 
-    string = f"Lähtö kotoa: {date} klo 7.55\nPaluu kotiin: {date} klo 16.00\nTyömatka {kaupunki}\nAsiakas Sathva"
-    copy_to_clipboard(string)
-    print(f"Lähtö kotoa: {date} klo 7.55")
-    print(f"Paluu kotiin: {date} klo 16.00")
-    print(f"Työmatka {kaupunki}")
-    print("Asiakas Sathva")
-    input("Enter to continue")
+clear_console()
+kaupunki = input("Kaupunki: ") 
+string = f"Lähtö kotoa: {date} klo 7.55\nPaluu kotiin: {date} klo 16.00\nTyömatka {kaupunki}\nAsiakas Sathva"
+copy_to_clipboard(string)
+print(f"Lähtö kotoa: {date} klo 7.55")
+print(f"Paluu kotiin: {date} klo 16.00")
+print(f"Työmatka {kaupunki}")
+print("Asiakas Sathva")
+input("Enter to continue")
